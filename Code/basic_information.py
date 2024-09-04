568016d5f0049ef971c78c77d5737d59ab97b7a0
Nelson Lin <nelsonlin@naturint.com>
2024-09-04 14:43:20 +0800

    Integrate basic and comment code to calss and def.

--- Code/basic_information.py
+++ Code/basic_information.py
@@ -1,71 +1,72 @@
 import requests
 from bs4 import BeautifulSoup
 import numpy as np
-# !pip install webdriver-manager
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
-import time
 
+class EscapeGameScraper:
+    def __init__(self, workshop_ids):
+        self.workshop_ids = workshop_ids
+        self.workshop_urls = self._generate_workshop_urls()
+        self.game_urls = []
 
-# 整理工作室的網站連結
-# 將所有工作室網站url的後位數字謄寫下來
-workshop_url_last_number = ['2612','2656','2627','8119','2659','2626','2629','12541','2637','10586','2635','2617','2624','2630','4082','2667','2652','18812','2640','11445','10282','2633','4906','15315','11224','4936','20075','2662','2632','20496']
-# 建立一個空串列用來放完整的所有工作室網站連結
-workshop_url = []
-# 利用for迴圈把藤寫下來的數字轉換成實際連結
-for number in range(len(workshop_url_last_number)):
-    workshop_url.append('https://escape.bar/firm/'+workshop_url_last_number[number])
-np.save('workshop_url.npy',workshop_url) # 儲存整理好的工作室連結字典
-print(workshop_url)
+    def _generate_workshop_urls(self):
+        """根據工作室ID生成工作室網址。"""
+        workshop_urls = [f'https://escape.bar/firm/{id}' for id in self.workshop_ids]
+        np.save('01_workshop_url.npy', workshop_urls)
+        return workshop_urls
 
-# 整理所有遊戲的網站連結
-# 定義從工作室網站中抓取遊戲連結的函式
-def Game_url(url):
-    response = requests.get(str(url)) # 抓取「工作室網址」
-    soup = BeautifulSoup(response.text, "html.parser") # 將網址的HTML寫進程式
-    # 將網址中所有該工作室遊戲連結抓下來
-    total_url = soup.find_all('div',class_='chakra-card__header css-4pyn5f')
-    total_html=['https://escape.bar'+str(i.select_one('a').get('href')) for i in total_url]
-    return total_html # 回傳該工作室的所有遊戲連結
+    def fetch_game_urls(self):
+        """從工作室網址中獲取所有遊戲網址。"""
+        for workshop_url in self.workshop_urls:
+            self.game_urls.extend(self._scrape_game_urls_from_workshop(workshop_url))
+        np.save('02_game_url.npy', self.game_urls)
+        return self.game_urls
 
-# 建立一個空串列用來存放完整遊戲的網站連結
-game_url = []
-# 利用for迴圈把所有工作室網站連結下去跑函式，產生出各工作室中的遊戲連結
-for workshop in range(len(workshop_url)):
-    game_url.append(Game_url(workshop_url[workshop])) # 將各工作室中的遊戲連結加入到串列裡
-game_url = [j for i in game_url for j in i] # 將串列降成一維
-np.save('game_url.npy',game_url) # 儲存整理好的遊戲連結字典
-print(game_url)
+    def _scrape_game_urls_from_workshop(self, url):
+        """從單個工作室頁面爬取遊戲網址。"""
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, "html.parser")
+        total_url = soup.find_all('div', class_='chakra-card__header css-4pyn5f')
+        return ['https://escape.bar' + i.select_one('a').get('href') for i in total_url]
 
+    def fetch_game_information(self):
+        """從遊戲網址中獲取所有遊戲資訊。"""
+        escape_game = {}
+        for game_url in self.game_urls:
+            game_info = self._scrape_game_information(game_url)
+            escape_game[game_info['topic']] = game_info
+        np.save('03_escape_game.npy', escape_game)
+        return escape_game
 
-# 整理所有遊戲的資訊
-# 定義從遊戲網站中抓取遊戲資訊的函式
-def Game_information(url):
-    response = requests.get(str(url)) # 抓取「遊戲網址」
-    soup = BeautifulSoup(response.text, "html.parser") # 將網址的HTML寫進程式
-    title = soup.find("h1", class_="text-2xl lg:text-3xl font-black") # 將「遊戲名稱」抓下來
-    money = soup.find('p',class_='mb-2 pl-4 leading-normal text-sm lg:text-base whitespace-pre-wrap') # 將「遊戲價錢」抓下來
-    content = soup.find('article',class_='ContentBlock_contentStyle__jR1s5')  # 將「遊戲說明」抓下來
-    Type = soup.find_all('b',class_='chakra-text css-0',limit=3) # 將「遊戲的關鍵字」抓下來，最多三個關鍵字
+    def _scrape_game_information(self, url):
+        """從單個遊戲頁面爬取遊戲資訊。"""
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, "html.parser")
+        title = soup.find("h1", class_="text-2xl lg:text-3xl font-black").getText()
+        money = soup.find('p', class_='mb-2 pl-4 leading-normal text-sm lg:text-base whitespace-pre-wrap').getText()
+        content = soup.find('article', class_='ContentBlock_contentStyle__jR1s5').getText()
+        Type = [t.getText() for t in soup.find_all('b', class_='chakra-text css-0', limit=3)]
+        
+        return {
+            'topic': title,
+            'information': money,
+            'content': content,
+            'type': Type,
+            'url': url
+        }
 
-    # 將抓下來的所有資訊，整理成一個字典，即為該遊戲的資訊
-    game_dict = {'topic':title.getText(),
-      'information':money.getText(),
-      'content':content.getText(),
-      'type':[t.getText() for t in Type],
-      'url':url}
-    return game_dict  # 回傳該遊戲資訊(字典型態)
+# Example usage
+workshop_ids = [
+    '2612', '2656', '2627', '8119', '2659', '2626', '2629', '12541', 
+    '2637', '10586', '2635', '2617', '2624', '2630', '4082', '2667', 
+    '2652', '18812', '2640', '11445', '10282', '2633', '4906', '15315', 
+    '11224', '4936', '20075', '2662', '2632', '20496'
+]
 
-# 建立一個空字典，用來存放所有遊戲的資訊
-escape_game = {}
-# 利用for迴圈將爬出來的所有遊戲資訊打包成字典
-for num in range(len(game_url)):
-    escape = Game_information(game_url[num]) # 將每個遊戲的網站連結丟入Game_information函式
-    escape_game[escape['topic']] = escape # 利用該遊戲的名稱當成key值;所有內容當成value值
-np.save('escape_game.npy',escape_game) # 儲存整理好的遊戲字典
+scraper = EscapeGameScraper(workshop_ids)
+scraper.fetch_game_urls()
+escape_game_data = scraper.fetch_game_information()
 
-Escape_games = np.load("escape_game.npy", allow_pickle='TRUE')
+# Load saved game data
+Escape_games = np.load("escape_game.npy", allow_pickle=True)
 print(Escape_games)
 print(type(Escape_games))
--- Code/comment_keyword.py
+++ Code/comment_keyword.py
@@ -1,72 +1,89 @@
-# 爬取遊戲評論
-# 定義動態網頁爬蟲，爬取遊戲近幾筆評論
-def Critical_catch(url):
-  try:
-    options = Options()
-    options.add_argument("--disable-notifications")  # 取消所有的alert彈出視窗
-    browser = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options) # 利用ChromeDriver爬取
-    browser.get(str(url)) # 輸入欲爬取網址
-    time.sleep(15) # 強制等待15秒，因為網頁會有幾秒的廣告時間
-    area = browser.find_element_by_id("tabs-:r6:--tab-1") # 找到「玩家評論」的按鈕
-    area.click() # 點選「玩家評論」的按鈕
-    soup = BeautifulSoup(browser.page_source, "html.parser") # 將點選完的網站HTML記錄下來，方便找到下一行的class名稱
-    nextpage = browser.find_element(By.CLASS_NAME,'chakra-button.css-wubf5g') # 找到「其他評論」的按鈕
-    # 持續點擊「其他評論」的按鈕，以利跑出更多評論(最多30下)
-    for i in range(30):
-      try:
-        nextpage.click()
-      except:
-        pass
-    time.sleep(3) # 強制等待3秒
-    soup2 = BeautifulSoup(browser.page_source, "html.parser") # 將點選完的網站HTML記錄下來，方便找到下一行的class名稱
-    elements = soup2.find_all("p", class_="text-gray-800 text-sm whitespace-pre-line") # 找到玩家評論的每條內容
-    critical = [el.getText() for el in elements] # 將每條評論內容依序讀到串列裡面
-  except:
-    critical = 'NaN'  # 若以上有問題的話則先輸入NaN註記
-  return critical # 回傳該遊戲抓取的玩家評論串列
-
-# 匯入之前抓好的所有遊戲網址
-game_url = np.load("game_url.npy", allow_pickle='TRUE')
-
-# 建立一個空字典，用來存放每個遊戲抓到的每個評論
-game_url_critical = {}
-# 利用for迴圈將每個遊戲網址輸入到動態爬蟲函式，將玩家評論爬下來
-for url in game_url:
-    critical_list = Critical_catch(str(url))
-    game_url_critical[url] = critical_list # 將爬好的玩家評論，整理成串列當成該遊戲url的value值存入字典中
-np.save('game_url_critical.npy',game_url_critical) # 儲存遊戲玩家評論字典
-
-
-# 整理評論成關鍵字
-# 匯入jieba套件
-# ! pip install -U jieba
+import time
+import numpy as np
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
 import jieba
 import jieba.analyse
-
-# 定義關鍵字擷取函式
-def Critical_keyword(critical):
-  topK = 10
-  tags = jieba.analyse.extract_tags(str(critical), topK=topK)
-  return tags
-
-# 匯入資料處理相關套件
-import numpy as np
 from collections import Counter
 
-# 匯入遊戲評論資料
-game_url_critical = np.load("game_url_critical.npy", allow_pickle='TRUE').item()
+class GameReviewScraper:
+    def __init__(self):
+        self.driver = None  # 初始化WebDriver為None
 
-# 將遊戲評論丟入關鍵字擷取函式，並產生出每個遊戲的評論關鍵字
-for url in game_url_critical.keys():
-  keypoint = [Critical_keyword(critical) for critical in game_url_critical[url]] # 以遊戲為單位將所有評論依序產生關鍵字，將產生後的關鍵字整理成一個串列
-  keypoint_new = [item for bot in keypoint for item in bot] # 將關鍵字串列降維
-  element_counts = Counter(keypoint_new) # 計算每個值出現的次數
-  most_common_values = element_counts.most_common(5) # 找到10個重複最多的值以及次數
-  key_keyword = [] # 建立一個空串列來放最終的評論關鍵字
-  for value, count in most_common_values: # 利用for迴圈將找到10個重複最多的關鍵字放入到串列中
-    key_keyword.append(value)
-  game_url_critical[url] = key_keyword # 將原本該遊戲的評論替換成最終評論關鍵字
+    def start_browser(self):
+        """啟動瀏覽器並設置選項。"""
+        options = Options()
+        options.add_argument("--disable-notifications")  # 取消所有的通知彈窗
+        self.driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
 
-# 儲存最新的評論關鍵字
-game_critical_keyword = game_url_critical
-np.save('game_critical_keyword.npy',game_critical_keyword)
+    def stop_browser(self):
+        """關閉瀏覽器。"""
+        if self.driver:
+            self.driver.quit()
+
+    def fetch_reviews(self, url):
+        """爬取指定URL的遊戲評論。"""
+        try:
+            self.driver.get(url)  # 打開指定的網址
+            time.sleep(15)  # 等待網頁加載完成，包括廣告時間
+            area = self.driver.find_element(By.ID, "tabs-:r6:--tab-1")  # 找到「玩家評論」按鈕
+            area.click()  # 點擊「玩家評論」按鈕
+            nextpage = self.driver.find_element(By.CLASS_NAME, 'chakra-button.css-wubf5g')  # 找到「其他評論」按鈕
+            
+            # 持續點擊「其他評論」按鈕，最多點擊30次
+            for _ in range(30):
+                try:
+                    nextpage.click()
+                except:
+                    pass
+            
+            time.sleep(3)  # 等待頁面更新
+            soup2 = BeautifulSoup(self.driver.page_source, "html.parser")  # 解析更新後的HTML
+            elements = soup2.find_all("p", class_="text-gray-800 text-sm whitespace-pre-line")  # 找到所有評論元素
+            reviews = [el.get_text() for el in elements]  # 提取每條評論的文本
+        except:
+            reviews = ['NaN']  # 如果出現錯誤，返回'NaN'
+        return reviews
+
+class KeywordExtractor:
+    @staticmethod
+    def extract_keywords(reviews, topK=10):
+        """從評論中提取關鍵字。"""
+        all_reviews = ' '.join(reviews)  # 將所有評論合併成一個字符串
+        return jieba.analyse.extract_tags(all_reviews, topK=topK)  # 提取前topK個關鍵字
+
+def main():
+    """主函式，控制爬取和關鍵字提取的流程。"""
+    scraper = GameReviewScraper()
+    scraper.start_browser()  # 啟動瀏覽器
+
+    # 匯入遊戲網址並爬取評論
+    game_urls = np.load("game_url.npy", allow_pickle=True)
+    game_reviews = {}
+
+    for url in game_urls:
+        reviews = scraper.fetch_reviews(str(url))  # 爬取評論
+        game_reviews[url] = reviews  # 將評論存入字典中
+
+    scraper.stop_browser()  # 關閉瀏覽器
+    np.save('04_game_url_critical.npy', game_reviews)  # 儲存爬取的評論
+
+    keyword_extractor = KeywordExtractor()
+    game_reviews = np.load("game_url_critical.npy", allow_pickle=True).item()
+
+    # 提取每個遊戲的評論關鍵字
+    for url in game_reviews.keys():
+        keypoint = [keyword_extractor.extract_keywords([review]) for review in game_reviews[url]]
+        keypoint_flat = [item for sublist in keypoint for item in sublist]  # 扁平化關鍵字列表
+        element_counts = Counter(keypoint_flat)  # 計算每個關鍵字的出現次數
+        most_common_values = element_counts.most_common(5)  # 找到出現最多的前5個關鍵字
+        key_keywords = [value for value, _ in most_common_values]  # 提取前5個關鍵字
+        game_reviews[url] = key_keywords  # 更新遊戲的評論關鍵字
+
+    np.save('05_game_critical_keyword.npy', game_reviews)  # 儲存最終的關鍵字
+
+if __name__ == "__main__":
+    main()
